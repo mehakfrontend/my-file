d9afaf961b652b21f24406c08327565c4b7fc1cf
mehak <mehakfrontenddev@gmail.com>
2024-11-25 06:53:00 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import About from "./pages/about";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
